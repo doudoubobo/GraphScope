c6903f33ee97ee6cf5c0ad2088a2bf9f7e0faa96
Lei Wang <jiede.wl@alibaba-inc.com>
2024-10-24 10:39:37 +0800

    rebase
    Signed-off-by: Lei Wang <jiede.wl@alibaba-inc.com>

--- interactive_engine/executor/ir/graph_proxy/src/apis/mod.rs
+++ interactive_engine/executor/ir/graph_proxy/src/apis/mod.rs
@@ -26,4 +26,6 @@ pub use graph::element::{
 };
 pub use graph::{read_id, write_id, Direction, QueryParams, ID};
 pub use read_graph::{from_fn, get_graph, register_graph, ReadGraph, Statement};
+pub use read_graph::{replace_read_version, get_read_version, replace_grin_partitioned_graph, get_grin_partitioned_graph, replace_grin_graph, get_grin_graph};
+pub use read_graph::{replace_process_partition_lists, get_process_partition_lists, replace_server_index, get_server_index};
 pub use write_graph::WriteGraphProxy;
--- interactive_engine/executor/ir/graph_proxy/src/apis/read_graph.rs
+++ interactive_engine/executor/ir/graph_proxy/src/apis/read_graph.rs
@@ -15,8 +15,12 @@
 
 use std::sync::atomic::{AtomicPtr, Ordering};
 use std::sync::Arc;
+use std::sync::Mutex;
+use std::collections::HashMap;
 
 use ir_common::LabelId;
+use grin::grin::GrinGraph;
+use grin::grin::GrinPartitionedGraph;
 
 use crate::apis::graph::PKV;
 use crate::apis::{Direction, Edge, QueryParams, Vertex, ID};
@@ -92,9 +96,26 @@ pub trait ReadGraph: Send + Sync {
     fn get_primary_key(&self, id: &ID) -> GraphProxyResult<Option<PKV>>;
 }
 
+pub struct GrinPartitionedGraphPtr(GrinPartitionedGraph);
+unsafe impl Send for GrinPartitionedGraphPtr {}
+pub struct GrinGraphPtr(GrinGraph);
+unsafe impl Send for GrinGraphPtr {}
+
 lazy_static! {
     /// GRAPH_PROXY is a raw pointer which can be safely shared between threads.
     pub static ref GRAPH_PROXY: AtomicPtr<Arc<dyn ReadGraph >> = AtomicPtr::default();
+
+    /// process_partition_lists is a HashMap<u32, Vec<u32>>
+    pub static ref PROCESS_PARTITION_LISTS: std::sync::RwLock<HashMap<u32, Vec<u32>>> = std::sync::RwLock::new(HashMap::new());
+    /// server_index is a u32
+    pub static ref SERVER_INDEX: std::sync::RwLock<u32> = std::sync::RwLock::new(0);
+    /// read_version is a u32
+    pub static ref READ_VERSION: std::sync::RwLock<u32> = std::sync::RwLock::new(0);
+    /// grin_partitioned_graph is a GrinPartitionedGraph (i.e., a raw pointer)
+    pub static ref GRIN_PARTITIONED_GRAPH: Mutex<GrinPartitionedGraphPtr> = Mutex::new(GrinPartitionedGraphPtr(std::ptr::null_mut()));
+    /// grin_graph is a GrinGraph (i.e., a raw pointer)
+    pub static ref GRIN_GRAPH: Mutex<GrinGraphPtr> = Mutex::new(GrinGraphPtr(std::ptr::null_mut()));
+
 }
 
 pub fn register_graph(graph: Arc<dyn ReadGraph>) {
@@ -109,4 +130,54 @@ pub fn get_graph() -> Option<Arc<dyn ReadGraph>> {
     } else {
         Some(unsafe { (*ptr).clone() })
     }
+}
+
+pub fn replace_read_version(new_version: u32) {
+    let mut read_version = READ_VERSION.write().unwrap();
+    *read_version = new_version;
+}
+
+pub fn get_read_version() -> u32 {
+    let read_version = READ_VERSION.read().unwrap();
+    *read_version
+}
+
+pub fn replace_grin_graph(new_graph: GrinGraph) {
+    let mut grin_graph = GRIN_GRAPH.lock().unwrap();
+    grin_graph.0 = new_graph;
+}
+
+pub fn get_grin_graph() -> GrinGraph {
+    let grin_graph = GRIN_GRAPH.lock().unwrap();
+    grin_graph.0.clone()
+}
+
+pub fn replace_grin_partitioned_graph(new_graph: GrinPartitionedGraph) {
+    let mut grin_partitioned_graph = GRIN_PARTITIONED_GRAPH.lock().unwrap();
+    grin_partitioned_graph.0 = new_graph;
+}
+
+pub fn get_grin_partitioned_graph() -> GrinPartitionedGraph {
+    let grin_partitioned_graph = GRIN_PARTITIONED_GRAPH.lock().unwrap();
+    grin_partitioned_graph.0.clone()
+}
+
+pub fn replace_process_partition_lists(new_map: HashMap<u32, Vec<u32>>) {
+    let mut global_map = PROCESS_PARTITION_LISTS.write().unwrap();
+    *global_map = new_map;
+}
+
+pub fn get_process_partition_lists() -> HashMap<u32, Vec<u32>> {
+    let global_map = PROCESS_PARTITION_LISTS.read().unwrap();
+    global_map.clone()
+}
+
+pub fn replace_server_index(new_index: u32) {
+    let mut server_index = SERVER_INDEX.write().unwrap();
+    *server_index = new_index;
+}
+
+pub fn get_server_index() -> u32 {
+    let server_index = SERVER_INDEX.read().unwrap();
+    *server_index
 }
